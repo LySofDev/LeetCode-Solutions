3a204349e6983ba7c8a6509681ae033c1c719e9d
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-04 16:26:43 -0400

    Solved p2 - Add Two Numbers

--- P2-AddTwoNumbers.py
+++ P2-AddTwoNumbers.py
@@ -1,7 +1,11 @@
 '''
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You are given two non-empty linked lists representing two non-negative 
+integers. The digits are stored in reverse order and each of their nodes
+contain a single digit. Add the two numbers and return it as a linked 
+list.
 
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+You may assume the two numbers do not contain any leading zero, except 
+the number 0 itself.
 
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
@@ -9,9 +13,63 @@ Output: 7 -> 0 -> 8
 import unittest as ut
 
 
-def main(nsx, nsy):
-	pass
+def safely_get(numbers, index):
+	try:
+		return numbers[index]
+	except IndexError:
+		return 0
 
+
+def main(nsx, nsy):
+	# Set limit to length of longest list
+	if len(nsx) > len(nsy):
+		limit = len(nsx)
+	else:
+		limit = len(nsy)
+		
+	# Set index to 0
+	index = 0
+	
+	# Set carry to 0
+	carry = 0
+	
+	# Set sum list to empty list
+	nss = []
+	
+	# While numbers still exist in either list
+	while index < limit:
+		
+		# Get digit by index or 0 if out of bounds
+		nx = safely_get(nsx, index)
+		ny = safely_get(nsy, index)
+		
+		# Increment index by 1
+		index += 1
+		
+		# Get digit sum with carry
+		ns = nx + ny + carry
+		
+		# If sum is two digits
+		if ns > 9:
+			
+			# Append last digit to result list
+			nss.append(ns % 10)
+			
+			# Set first digit as carry value
+			carry = ns // 10
+			
+		# If sum is one digit
+		else:
+			
+			# Append digit to result list
+			nss.append(ns)
+			
+			# Set carry value to 0
+			carry = 0
+			
+	# Return resulting list
+	return nss
+			
 
 class Tests (ut.TestCase):
 	
