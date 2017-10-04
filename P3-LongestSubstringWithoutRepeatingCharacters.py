a92f784409b5cf44333b7d98109b9b50b5659f0d
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-04 16:50:15 -0400

    Initialized problem 3

---
+++ P3-LongestSubstringWithoutRepeatingCharacters.py
@@ -0,0 +1,30 @@
+'''
+Longest Substring Without Repeating Characters
+
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+'''
+import unittest as ut
+
+
+def main():
+	# SOLUTION
+	pass
+	
+
+class Tests (ut.TestCase):
+
+	def testA(self):
+		# GIVEN TEST
+		pass
+
+
+if __name__ == '__main__':
+	ut.main()
