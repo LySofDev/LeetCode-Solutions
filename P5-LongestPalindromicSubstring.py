18e7991b60e562470609f85f9a8a7c842e9ce1a0
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-04 20:18:50 -0400

    Initialized problem 5

---
+++ P5-LongestPalindromicSubstring.py
@@ -0,0 +1,36 @@
+'''
+5. Longest Palindromic Substring
+
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example:
+
+Input: "babad"
+
+Output: "bab"
+
+Note: "aba" is also a valid answer.
+
+Example:
+
+Input: "cbbd"
+
+Output: "bb"
+'''
+import unittest as ut
+
+
+def main():
+	# SOLUTION
+	pass
+
+
+class Tests (ut.TestCase):
+
+	def testA(self):
+		# GIVEN TEST
+		pass
+
+
+if __name__ == '__main__':
+	ut.main()
--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 1. [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/description/)
 1. [Longest Substring Without Repeating Characters](https://leetcode.com/problems/longest-substring-without-repeating-characters/description/)
 1. [Median Of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays/description/)
+1. [Longest Palindromic Substring](https://leetcode.com/problems/longest-palindromic-substring/description/)
