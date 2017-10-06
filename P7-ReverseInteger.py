30bf3d95afe5b7a9ded0912bba388fb982bfbd0f
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-06 16:04:13 -0400

    Added given test cases

--- P7-ReverseInteger.py
+++ P7-ReverseInteger.py
@@ -1,10 +1,21 @@
 '''
-<PROBLEM DESCRIPTION HERE>
+7. Reverse Integer
+
+Reverse digits of an integer.
+
+Example1: x = 123, return 321
+Example2: x = -123, return -321
+
+click to show spoilers.
+
+Note:
+The input is assumed to be a 32-bit signed integer. Your function should
+return 0 when the reversed integer overflows.
 '''
 import unittest as ut
 
 
-def main():
+def main(x):
 	# SOLUTION
 	pass
 	
@@ -12,9 +23,17 @@ def main():
 class Tests (ut.TestCase):
 
 	def testA(self):
-		# GIVEN TEST
-		pass
-
+		x = 123
+		expected = 321
+		result = main(x)
+		self.assertEqual(expected, result)
+	
+	def testB(self):
+		x = -123
+		expected = -321
+		result = main(x)
+		self.assertEqual(expected, result)
+		
 
 if __name__ == '__main__':
 	ut.main()
