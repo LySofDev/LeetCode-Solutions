e3985bf42f7dee70a77797ad6aa136c2b4a2e4a7
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-06 22:31:21 -0400

    Solved problem 8 - String to intger

--- P8-StringToInteger.py
+++ P8-StringToInteger.py
@@ -15,9 +15,51 @@ def main(cs):
 	cs: String -> Text representing an integer
 	return: Integer -> Number represented by text
 	'''
-	# SOLUTION
-	pass
+	r = 0
+	n = False
+	s = False
+	f = False
 	
+	for c in cs:
+		
+		if c == '-':
+			
+			if s:
+				
+				return 0
+				
+			else:
+				
+				s = True
+				
+				n = True
+				
+		elif c.isnumeric():
+			
+			if not s:
+				
+				s = True
+				
+			if f:
+				
+				return 0
+			
+			r = r * 10 + int(c)
+			
+		elif c == ' ' and s:
+			
+			f = True
+			
+		elif c != ' ':
+			
+			return 0
+			
+	if n:
+		
+		r = -r
+			
+	return r
+			
 
 class Tests (ut.TestCase):
 
@@ -47,7 +89,19 @@ class Tests (ut.TestCase):
 		
 	def testE(self):
 		s = "fail"
-		expected = None
+		expected = 0
+		result = main(s)
+		self.assertEqual(expected, result)
+		
+	def testF(self):
+		s = "-45-3"
+		expected = 0
+		result = main(s)
+		self.assertEqual(expected, result)
+		
+	def testG(self):
+		s = "34 8"
+		expected = 0
 		result = main(s)
 		self.assertEqual(expected, result)
 
