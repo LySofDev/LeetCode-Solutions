1966ec2c86ec722dbc3101068eedd51d5dfb0f16
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-06 22:07:46 -0400

    Initialized problem 8

---
+++ P8-StringToInteger.py
@@ -0,0 +1,20 @@
+'''
+<PROBLEM DESCRIPTION HERE>
+'''
+import unittest as ut
+
+
+def main():
+	# SOLUTION
+	pass
+	
+
+class Tests (ut.TestCase):
+
+	def testA(self):
+		# GIVEN TEST
+		pass
+
+
+if __name__ == '__main__':
+	ut.main()
