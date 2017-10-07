e50d946c1c4784db518e2f70e42a8abc6ed8e38e
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-06 22:09:44 -0400

    Added Problem description

--- P8-StringToInteger.py
+++ P8-StringToInteger.py
@@ -1,5 +1,11 @@
 '''
-<PROBLEM DESCRIPTION HERE>
+8. Strign To Integer
+
+Implement atoi to convert a string to an integer.
+
+Hint: Carefully consider all possible input cases. If you want a challenge, please do not see below and ask yourself what are the possible input cases.
+
+Notes: It is intended for this problem to be specified vaguely (ie, no given input specs). You are responsible to gather all the input requirements up front.
 '''
 import unittest as ut
 
