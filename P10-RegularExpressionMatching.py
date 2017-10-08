f0d5b662e88d3034d5b787e47731b18277317f25
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-08 16:38:47 -0400

    Added given test cases and function description

--- P10-RegularExpressionMatching.py
+++ P10-RegularExpressionMatching.py
@@ -1,12 +1,33 @@
 '''
-<PROBLEM DESCRIPTION HERE>
+10. Regular Expression Matching
+
+Implement regular expression matching with support for '.' and '*'.
+
+'.' Matches any single character.
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+
+The function prototype should be:
+bool isMatch(const char *s, const char *p)
+
+Some examples:
+isMatch("aa","a") → false
+isMatch("aa","aa") → true
+isMatch("aaa","aa") → false
+isMatch("aa", "a*") → true
+isMatch("aa", ".*") → true
+isMatch("ab", ".*") → true
+isMatch("aab", "c*a*b") → true
 '''
 import unittest as ut
 
 
-def main():
+def main(cs, r):
 	'''
-	return: None
+	cs: String -> Original text
+	r: String -> Regular expression to match
+	return: Boolean -> True if regular expression is matched in Original text
 	'''
 	# SOLUTION
 	pass
@@ -15,8 +36,53 @@ def main():
 class Tests (ut.TestCase):
 
 	def testA(self):
-		# GIVEN TEST
-		pass
+		string = "aa"
+		regex = "a"
+		expected = False
+		result = main(string, regex)
+		self.assertEqual(expected, result)
+
+	def testB(self):
+		string = "aa"
+		regex = "aa"
+		expected = True
+		result = main(string, regex)
+		self.assertEqual(expected, result)
+
+	def testC(self):
+		string = "aaa"
+		regex = "aa"
+		expected = False
+		result = main(string, regex)
+		self.assertEqual(expected, result)
+
+	def testD(self):
+		string = "aa"
+		regex = "a*"
+		expected = True
+		result = main(string, regex)
+		self.assertEqual(expected, result)
+
+	def testE(self):
+		string = "aa"
+		regex = ".*"
+		expected = True
+		result = main(string, regex)
+		self.assertEqual(expected, result)
+
+	def testF(self):
+		string = "ab"
+		regex = ".*"
+		expected = True
+		result = main(string, regex)
+		self.assertEqual(expected, result)
+
+	def testE(self):
+		string = "aab"
+		regex = "c*a*b*"
+		expected = True
+		result = main(string, regex)
+		self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
