f7a8980853a70034c49845343c6747080eaf5102
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-08 16:32:37 -0400

    Initiated problem 10

---
+++ P10-RegularExpressionMatching.py
@@ -0,0 +1,23 @@
+'''
+<PROBLEM DESCRIPTION HERE>
+'''
+import unittest as ut
+
+
+def main():
+	'''
+	return: None
+	'''
+	# SOLUTION
+	pass
+
+
+class Tests (ut.TestCase):
+
+	def testA(self):
+		# GIVEN TEST
+		pass
+
+
+if __name__ == '__main__':
+	ut.main()
