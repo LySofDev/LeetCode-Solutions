abd6d96cb23ac65371b2cc1ed9ff96dc08ec68f2
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-08 18:38:41 -0400

    Passed first three tests

--- P10-RegularExpressionMatching.py
+++ P10-RegularExpressionMatching.py
@@ -29,7 +29,30 @@ def main(cs, r):
 	r: String -> Regular expression to match
 	return: Boolean -> True if regular expression is matched in Original text
 	'''
-	pass
+	# Set regex index to 0
+	ri = 0
+
+	# While original text still has characters and regex index is in range
+	while len(cs) and len(r) > ri:
+
+		# If first character in original text match current regex character
+		if cs[0] == r[ri]:
+
+			# Remove first character from original text
+			cs = cs[1:]
+
+			# Increment regex index by 1
+			ri += 1
+
+		# If first character in original text doesn't match current regex
+		# character
+		else:
+
+			return False
+
+	# If regex index is length of regex and original text is empty
+	return len(r) <= ri and not len(cs)
+
 
 
 class Tests (ut.TestCase):
