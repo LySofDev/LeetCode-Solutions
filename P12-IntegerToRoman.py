9c8b532b4977403da113b1e79539bcd53ca708ab
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-08 20:55:38 -0400

    removed redundancies from test

--- P12-IntegerToRoman.py
+++ P12-IntegerToRoman.py
@@ -18,21 +18,18 @@ def main():
 
 class Tests (ut.TestCase):
 
-	def setUp(self):
-		self.numbers = [
+	def testAll(self):
+		numbers = [
 			1093, 282, 2471, 417, 3272, 477, 3643, 3771, 1019
 		]
-		self.expected = [
+
+		romans = [
 			"MXCIII", "CCLXXXII", "MMCDLXXI", "CDXVII", "MMMCCLXXII",
 			"CDLXXVII", "MMMDCXLIII", "MMMDCCLXXI", "MXIX"
 		]
 
-	def testAll(self):
-		for i in range(len(self.numbers)):
-			number = self.numbers[i]
-			expected = self.expected[i]
-			result = main(number)
-			self.assertEqual(expected, result)
+		for i in range(len(numbers)):
+			self.assertEqual(main(numbers[i]), romans[i])
 
 
 if __name__ == '__main__':
