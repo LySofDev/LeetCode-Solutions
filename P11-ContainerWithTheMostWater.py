09383df6313d70bd33a388252417fbcf469703f0
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-09 13:03:36 -0400

    Solved problem 11

--- P11-ContainerWithTheMostWater.py
+++ P11-ContainerWithTheMostWater.py
@@ -13,9 +13,44 @@ def main(hs):
 	hs: Integer[] -> List of heights for lines
 	return: Integer -> Greatest possible area
 	'''
-	# SOLUTION
-	pass
+	# Initialize maximum area to 0
+	ma = 0
 
+	# Initialize left index to 0
+	li = 0
+
+	# Initialize right index to last possible index
+	ri = len(hs) - 1
+
+	# While left index is less than right index
+	while li < ri:
+
+		# Width is equal to the difference between left and right index
+		w = ri - li
+
+		# Height is shortest length between left and right height
+		h = min(hs[li], hs[ri])
+
+		# If left height is shorter than right height
+		if h == hs[li]:
+
+			# Increment left index by one
+			li += 1
+
+		# If right height is shorter than left height
+		else:
+
+			# Decrement right index by one
+			ri -= 1
+
+		# Area is equal to width times height
+		a = w * h
+
+		# Max height is greatest of area and maximum area
+		ma = max(ma, a)
+
+	# Return maximum area
+	return ma
 
 class Tests (ut.TestCase):
 
