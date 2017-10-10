84f9f1b1d78d8a9ff4305c26f9248dd8cbfdedfb
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-09 23:35:55 -0400

    Initiated prolbem 14 - longest common prefix

---
+++ P14-LongestCommonPrefix.py
@@ -0,0 +1,33 @@
+'''
+14. Longest Common Prefix
+
+Write a function to find the longest common prefix string amongst an array of strings.
+'''
+import unittest as ut
+
+
+def main():
+	'''
+	return: None
+	'''
+	# SOLUTION
+	pass
+
+
+class Tests (ut.TestCase):
+
+	def testA(self):
+		strings = [ "hello", "hell", "helicopter" ]
+		expected = "hel"
+		result = main(strings)
+		self.assertEqual(expected, result)
+
+	def testB(self):
+		strings = [ "ruins", "run", "rum" ]
+		expected = "ru"
+		result = main(strings)
+		self.assertEqual(expected, result)
+		
+
+if __name__ == '__main__':
+	ut.main()
