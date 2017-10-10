0dc25730f45a25293a67eaf289c093a4b3fed709
Esteban Hernandez <hernandez.dev.services@gmail.com>
2017-10-10 00:07:48 -0400

    Solved problem 14

--- P14-LongestCommonPrefix.py
+++ P14-LongestCommonPrefix.py
@@ -11,8 +11,41 @@ def main(ss):
 	ss: String[] -> List of strings
 	return: String -> Longest common prefix
 	'''
-	# SOLUTION
-	pass
+	# Initiate longest common prefix to an empty string
+	lcp = ""
+
+	# Set character index to 0
+	i = 0
+
+	# Set limit to the shortest length of strings
+	l = min(map(lambda s: len(s), ss))
+
+	# While character index is less than length of shortest string
+	while i < l:
+
+		# Get the current character with the character index
+		cc = ss[0][i]
+
+		am = True
+
+		for s in ss:
+
+			if s[i] != cc:
+
+				am = False
+
+		if am:
+
+			lcp += cc
+
+			i += 1
+
+		else:
+
+			break
+
+	# Return the longest common prefix
+	return lcp
 
 
 class Tests (ut.TestCase):
@@ -26,6 +59,12 @@ class Tests (ut.TestCase):
 	def testB(self):
 		strings = [ "ruins", "run", "rum" ]
 		expected = "ru"
+		result = main(strings)
+		self.assertEqual(expected, result)
+
+	def testC(self):
+		strings = [ "hi", "high", "higgs" ]
+		expected = "hi"
 		result = main(strings)
 		self.assertEqual(expected, result)
 
